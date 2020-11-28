d56c8433ef97fcf83ff0cc900dfde3e858a74256
ladyofcode <thelab@ladyofcode.com>
2020-11-29 01:29:33 +1100

    Chat window (#14)
    * WIP message component
    
    * WIP components for chat window
    
    Co-authored-by: Logan Smithson <logan3713@gmail.com>

---
+++ frontend/src/components/chatWindow/avatar/avatar.tsx
@@ -0,0 +1,9 @@
+export function Avatar(props) {
+  return (
+    <div className="avatarBox">
+      <img className="avatar"></img>
+      <div className="downArrow"></div>
+      <div className="sideArrow"></div>
+    </div>
+  );
+}
