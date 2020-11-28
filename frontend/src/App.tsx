08a5b83bba5a21f9984c7145167208b402db7620
ladyofcode <tabs@ladyofcode.com>
2020-11-28 17:30:53 +1100

    Add basic registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home/Home';
+import Registration from './pages/Registration/Registration'
 
 function App() {
   return (
     <div className="App">
 
-      <Home />
+      <Registration />
       
     </div>
   );
